24114406cb8b3757cd076ed6d6d568608292c452
mxiyuan <mxiyuan@outlook.com>
2017-10-22 19:46:28 +0800

    add 001, 002 and 008

---
+++ cpp/008_rotate_string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /**
+     * @param str: a string
+     * @param offset: an integer
+     * @return: nothing
+     */
+    void rotateString(string &str,int offset){
+        if(!str.empty()) {
+            offset %= str.length();
+            reverse(str.begin(), str.begin() + str.length() - offset);
+            reverse(str.begin() + str.length() - offset, str.end());
+            reverse(str.begin(), str.end());
+        }
+    }
+};
