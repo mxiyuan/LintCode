072c884bef103424490745df50644d2f97daa598
mxiyuan <mxiyuan@outlook.com>
2017-10-31 20:25:12 +0800

    add 212.cpp

--- cpp/212_space_replacement.cpp
+++ cpp/212_space_replacement.cpp
@@ -5,24 +5,25 @@ public:
      * @param length: The true length of the string
      * @return: The true length of new string
      */
-    int replaceBlank(string &string, int length) {
-
+    int replaceBlank(char *str, int length) {
         const array<char, 3> to{'%', '2', '0'};
 
         int spaceCount = 0;
         for (int i = 0; i < length; ++i) {
-            if (string[i] == ' ') {
+            if (str[i] == ' ') {
                 ++spaceCount;
             }
         }
 
         const int new_length = length + (static_cast<int>(to.size()) - 1) * spaceCount;
         for (int i = length - 1, j = new_length - 1; i >= 0; --i) {
-            if (string[i] == ' ') {
-                j -= to.size();
-                copy(to.cbegin(), to.cend(), string.begin() + j + 1);
+            if (str[i] == ' ') {
+                int k = to.size() - 1;
+                while(k >= 0) {
+                    str[j--] = to[k--];
+                }
             } else {
-                string[j--] = string[i];
+                str[j--] = str[i];
             }
         }
         
