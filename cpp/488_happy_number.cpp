30889f52f58694bcd142db80c4fb19c64f7b3610
mxiyuan <mxiyuan@outlook.com>
2017-11-02 20:18:39 +0800

    add 488

--- cpp/488_happy_number.cpp
+++ cpp/488_happy_number.cpp
@@ -5,8 +5,9 @@ public:
      * @return: true if this is a happy number or false
      */
     bool isHappy(int n) {
-        // write your code here
-        function<int, int> nextNumber = [](int num) -> {
+        unordered_set<int> visited;
+
+        auto nextNumber = [](int num) -> int {
             int sum = 0;
             while(num) {
                 int rem = num % 10;
@@ -15,7 +16,7 @@ public:
             }
             return sum;
         };
-        unordered_set<int> visited;
+        
         while(n != 1 && !visited.count(n)) {
             visited.emplace(n);
             n = nextNumber(n);
@@ -23,5 +24,3 @@ public:
         return n == 1;
     }
 };
-
-//有错误
