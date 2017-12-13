161b529f16231add4e5acecf6121b1a130d6d53e
mxiyuan <mxiyuan@outlook.com>
2017-12-13 21:35:01 +0800

    modify 366

--- cpp/366_fibonacci.cpp
+++ cpp/366_fibonacci.cpp
@@ -1,20 +1,24 @@
 // Time:  O(n)
 // Space: O(1)
 
-class Solution{
-public:
+class Solution
+{
+  public:
     /**
      * @param n: an integer
      * @return an integer f(n)
      */
-    int fibonacci(int n) {
-        if(n < 1) {
+    int fibonacci(int n)
+    {
+        if (n < 1)
+        {
             return 0;
         }
 
         array<int, 3> fib{0, 1, 1};
 
-        for (int i = 3; i < n; ++i) {
+        for (int i = 3; i < n; ++i)
+        {
             fib[i % 3] = fib[(i - 1) % 3] + fib[(i - 2) % 3];
         }
 
