251b31b6c9ef8e348875600deec86d9bc7aba768
Xi Yuan <mxiyuan@outlook.com>
2018-03-15 21:34:10 +0800

    Solve 661

---
+++ Java/661_convert_bst_to_greater_tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of binary tree
+     * @return: the new root
+     */
+    private int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+    }
+}
