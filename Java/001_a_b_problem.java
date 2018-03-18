8b242084d8b91365761e69faf9789e3b1f9abf08
Xi Yuan <mxiyuan@outlook.com>
2018-03-18 17:07:52 +0800

    Solve 001 in Java

---
+++ Java/001_a_b_problem.java
@@ -0,0 +1,16 @@
+public class Solution {
+    /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: The sum of a and b
+     */
+    public int aplusb(int a, int b) {
+        while(b != 0) {
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            a = sum;
+            b = carry;
+        }
+        return a;
+    }
+}
