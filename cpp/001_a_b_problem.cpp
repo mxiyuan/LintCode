b8a19f3c3851452d003f42f91c3908c0f6bc04b3
Xi Yuan <mxiyuan@outlook.com>
2018-03-18 17:23:55 +0800

    Modify 001.cpp

--- cpp/001_a_b_problem.cpp
+++ cpp/001_a_b_problem.cpp
@@ -1,38 +1,33 @@
 // Time:  O(logn) = O(32)
 // Space: O(1)
 
-class Solution
-{
-  public:
+class Solution {
+public:
     /*
      * @param a: The first integer
      * @param b: The second integer
      * @return: The sum of a and b
      */
-    int aplusb(int a, int b)
-    {
-        while (b != 0)
-        {
-            int carry = a & b;
-            a ^= b;
-            b = carry << 1;
+    int aplusb(int a, int b) {
+        while (b != 0) {
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            a = sum;
+            b = carry;
         }
         return a;
     }
 };
 
-class Solution2
-{
-  public:
+class Solution2 {
+public:
     /*
      * @param a: The first integer
      * @param b: The second integer
      * @return: The sum of a and b
      */
-    int aplusb(int a, int b)
-    {
-        if (b == 0)
-        {
+    int aplusb(int a, int b) {
+        if (b == 0) {
             return a;
         }
         return aplusb(a ^ b, (a & b) << 1);
